3a02a90f0f2b2544caac6fb509c06de6f0522f1e
muhammadumer-ai <muhammadumer4519@gmail.com>
2026-02-18 17:51:41 +0500

    Update student_code.cpp

--- src/student_code.cpp
+++ src/student_code.cpp
@@ -9,17 +9,21 @@ int getIntValue() {
 
 float getFloatValue() {
     // Declare a variable var_float of type float and set its value to 10.75.
+    float var_float=10.75;
     //return var_float;
+    return var_float;
 }
 
 char getCharValue() {
     // Declare a variable var_char of type character and set its value to A.
+    char var_char='A';
     //return var_char;
+    return var_char;
 }
 
 int convertFloatToInt(float value) {
     // convert value to int.
-    return value;
+    return static_cast<int>(value);
 }
 
 /*
@@ -29,6 +33,8 @@ Formula:
     Celsius = (Fahrenheit - 32) * 5 / 9
 */
 float fahrenheitToCelsius(float fahrenheit) {
+    float celsius;
+    celsius=(fahrenheit - 32) *5/9;
     //return celsius;
-
+return celsius;
 }
